98a898cec670e5d980da35fc365524e91c861f2f
Victoria Kee <victoriakee95@gmail.com>
2020-06-18 09:23:11 -0500

    Declare balloon vars

---
+++ script.js
@@ -0,0 +1,47 @@
+const balloon0 = document.getElementById('balloon0');
+
+const balloon0 = document.getElementById('balloon1');
+
+const balloon0 = document.getElementById('balloon2');
+
+const balloon0 = document.getElementById('balloon3');
+
+const balloon0 = document.getElementById('balloon4');
+
+const balloon0 = document.getElementById('balloon5');
+
+const balloon0 = document.getElementById('balloon6');
+
+const balloon0 = document.getElementById('balloon7');
+
+const balloon0 = document.getElementById('balloon8');
+
+const balloon0 = document.getElementById('balloon9');
+
+const balloon0 = document.getElementById('balloon10');
+
+const balloon0 = document.getElementById('balloon11');
+
+const balloon0 = document.getElementById('balloon12');
+
+const balloon0 = document.getElementById('balloon13');
+
+const balloon0 = document.getElementById('balloon14');
+
+const balloon0 = document.getElementById('balloon15');
+
+const balloon0 = document.getElementById('balloon16');
+
+const balloon0 = document.getElementById('balloon17');
+
+const balloon0 = document.getElementById('balloon18');
+
+const balloon0 = document.getElementById('balloon19');
+
+const balloon0 = document.getElementById('balloon20');
+
+const balloon0 = document.getElementById('balloon21');
+
+const balloon0 = document.getElementById('balloon22');
+
+const balloon0 = document.getElementById('balloon23');
